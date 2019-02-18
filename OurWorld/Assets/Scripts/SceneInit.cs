08c2b7d9c78f12d528ef9f8d72662b202e340ff1
tonynap <tonynap@vt.edu>
2019-02-18 04:02:08 -0500

    PASSTHROUGH WORKING IN SIMULATED VRRRRRRRR.!!1!!!!!! YAY. YAY! next big hurdle will be getting it to work in real vrtk. and goddamn the bullshit of it recompiling constantly and having to continuously add openvr back is annoying as hell, but it's not the end of the world, it won't actually add that much time in the grand scheme of things, i'll have it in a dedicated window ready to toggle in a second or two whenever I want to play.

---
+++ OurWorld/Assets/Scripts/SceneInit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UnityEditor.PlayerSettings.
+        //UnityEngine.VR.VRSettings.LoadDeviceByName("OpenVR")        Debug.Log("Loading OpenVR");
+        UnityEngine.XR.XRSettings.LoadDeviceByName("OpenVR");
+        Debug.Log("Finished Loading OpenVR");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
