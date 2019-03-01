4a57a7bedb04f2b2b3d5c8aa37feb2a432a64c83
tonynap <tonynap@vt.edu>
2019-02-28 22:32:12 -0500

    collider mesh now updating in response to blend shapes

---
+++ OurWorld/Assets/Scenes/Levels/Realm 1 - Round 2/Blendshapes test/MeshColliderUpdater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshColliderUpdater : MonoBehaviour
+{
+    public int blendKeyIndex = 0;
+    // public float blendAmount = 100;
+
+    private SkinnedMeshRenderer render;
+    private MeshCollider meshCollider;
+
+    private void Start()
+    {
+        render = GetComponent<SkinnedMeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float blendWeight = render.GetBlendShapeWeight(blendKeyIndex);
+        render.SetBlendShapeWeight(blendKeyIndex, blendWeight);
+        Mesh bakeMesh = new Mesh();
+        render.BakeMesh(bakeMesh);
+        meshCollider.sharedMesh = bakeMesh;
+    }
+}
