78ec2c34b1d62f9491cab1851ad7b90d85ac0db6
tonynap <tonynap@vt.edu>
2019-03-02 15:27:26 -0500

    added MyBox inspector widgets library, not as git repo but as raw files

--- OurWorld/Assets/Scenes/Levels/Realm 1 - Round 2/Enemy Management/FollowSomeone.cs
+++ OurWorld/Assets/Scenes/Levels/Realm 1 - Round 2/Enemy Management/FollowSomeone.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using MyBox;
 
 public class FollowSomeone : MonoBehaviour
 {
+    [Tooltip("Will follow the main camera if this is checked. If not checked, specify a specific game object as a target.")]
     public bool followMainCamera = false;
+
+    [Tooltip("Specific game object to follow.")]
+    [ConditionalField("followMainCamera", false)]
     public GameObject target;
+
     private NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -18,6 +24,14 @@ public class FollowSomeone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        Vector3 dest;
+
+        if(followMainCamera) {
+            dest = Camera.main.transform.position;
+        } else {
+            dest = target.transform.position;
+        }
+
+        agent.SetDestination(dest);
     }
 }
